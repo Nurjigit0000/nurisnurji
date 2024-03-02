0778f2ffd1c61210a385de9c56c78588f529749c
Dosaliev <d080429@gmail.com.com>
2024-03-02 16:00:25 +0600

    фиксаация 9 нуржигит

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -23,16 +23,21 @@ const Footer = () => {
         <div className='mb-[7px] text-white'>GET IN TOUCH</div>
         <div className='mb-[7px] text-white'>Call: <span className=' our-color'>(405) 555-0128</span></div>
         <div className='mb-[7px] text-white'>Email: <span className=' our-color'>hello@createx.com</span></div>
-        <div>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" /> 
-          <img src="" alt="" />
-          <img src="" alt="" />
+        <div className='flex'>
+          <img src="/src/assets/footer-images/shape.svg" alt="" />
+          <img src="/src/assets/footer-images/shape inst.svg" alt="" />
+          <img src="/src/assets/footer-images/shape twitter.svg" alt="" />
+          <img src="/src/assets/footer-images/shape youtube.svg" alt="" />
+          <img src="/src/assets/footer-images/shape pinterest.svg" alt="" />
         </div>
       </div>
-      <div className='text-white ml-[120px] mt-[60px]'>DOWNLOAD OUR APP </div>
-
+      <div>
+        <div className='text-white ml-[120px] mt-[60px]'>DOWNLOAD OUR APP </div>
+        <div className='flex ml-[120px] mt-[20px]'>
+          <img src="/src/assets/footer-images/app-store.png" alt="" />
+          <img className='ml-[15px]' src="/src/assets/footer-images/google-play.png" alt="" />
+        </div>
+      </div>  
     </div>
   )
 }
